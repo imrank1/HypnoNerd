f950739a865f897405eb301b81b0d12e2ceda36b
imran khawaja <imrank1@gmail.com>
2014-07-27 21:26:19 -0700

    Initial Commit

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Imran Khawaja on 7/27/14.
+//  Copyright (c) 2014 imrankhawaja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoNerd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoNerd
+//
+//  Created by Imran Khawaja on 7/27/14.
+//  Copyright (c) 2014 imrankhawaja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
